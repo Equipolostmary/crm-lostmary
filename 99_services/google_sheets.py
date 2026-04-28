0cdee7edde0777ecf659c05667d206c949c31128
Equipolostmary <equipolostmary@gmail.com>
2026-04-28 14:34:04 +0200

    Add files via upload

---
+++ 99_services/google_sheets.py
@@ -0,0 +1,29 @@
+import gspread
+from google.oauth2.service_account import Credentials
+import streamlit as st
+import pandas as pd
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+def connect_to_sheets():
+    creds = Credentials.from_service_account_info(
+        st.secrets["google_credentials"], scopes=SCOPES
+    )
+    client = gspread.authorize(creds)
+    return client
+
+def get_all_sheets(spreadsheet_url):
+    client = connect_to_sheets()
+    spreadsheet = client.open_by_url(spreadsheet_url)
+
+    all_data = {}
+
+    for worksheet in spreadsheet.worksheets():
+        data = worksheet.get_all_records()
+        df = pd.DataFrame(data)
+        all_data[worksheet.title] = df
+
+    return all_data
